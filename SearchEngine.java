dccf161fdf417544118ebd91d2a9d24f92e3fd92
yal103 <yal103@ucsd.edu>
2023-04-20 14:41:46 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,38 +1,59 @@
-// import java.io.IOException;
-// import java.net.URI;
-// import java.security.cert.PKIXBuilderParameters;
+import java.io.IOException;
+import java.net.URI;
 
-// class SearchEngine implements URLHandler{
-    
-//     String[] strArr = new String[];
+class StringHandler implements URLHandler {
 
-//     public String handleRequest(URI url) {
-//         if (url.getPath().equals("/")) {
-//             return str;
-//         }
-//         else if (url.getPath().contains("/add")){
-//             System.out.println("Path: " + url.getPath());
-//             String[] parameters = url.getQuery().split("=");
-//             if (parameters[0].equals("s")) {
-//                 str = str + parameters[1] + "\n";
-//                 return parameters[1] + "has been added.";
-//             }
-//         else if (url.getPath().contains("/search")) {
-//             System.out.println("Path: " + url.getPath());
-//             String[] parameters = url.getQuery().split("=");
-//             if (parameters[0].equals("s")) {
-                
-//             }
-//         }
+    String str = "";
+    String[] arrStr = new String[999999999];
+    int num = 0;
 
-//             // System.out.println("Path: " + url.getPath());
-//             // if (url.getPath().contains("/add")) {
-//             //     String[] parameters = url.getQuery().split("=");
-//             //     if (parameters[0].equals("count")) {
-//             //         num += Integer.parseInt(parameters[1]);
-//             //         return String.format("Number increased by %s! It's now %d", parameters[1], num);
-//             //     }
-//             // }
-//         }
-//     }
-// }
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("List of Words: \n%s", str);
+        }
+        else if (url.getPath().contains("/add")){
+            System.out.println("Path: " + url.getPath());
+            String[] parameters1 = url.getQuery().split("=");
+            if (parameters1[0].equals("s") && num < 999999999) {
+                str = str + parameters1[1] + "\n";
+                arrStr[num] = parameters1[1];
+                num++;
+                return parameters1[1] + " has been added.";
+            }
+        }
+        else if (url.getPath().contains("/search")) {
+            System.out.println("Path: " + url.getPath());
+            String[] parameters2 = url.getQuery().split("=");
+            if (parameters2[0].equals("s")) {
+                String returnStr = "The added words containing " + "\"" + parameters2[1] + "\" are: \n";
+                for (String string: arrStr) {
+                    if (string != null) {
+                        if (string.contains(parameters2[1])) {
+                            returnStr += (string + "\n");
+                        }
+                    }
+                    
+                }
+                return returnStr;
+            }
+        }
+        return "404 Not Found!";
+        
+        
+    }
+
+}
+
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new StringHandler());
+    }
+}
